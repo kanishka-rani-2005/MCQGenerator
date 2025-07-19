efc6a18b46f56322b8d2e15b87fd666963c9be43
Kanishka Rani <kanishka22043@gmail.com>
2025-07-19 19:54:34 +0530

    Update requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 
 setup(
     name='MCQGENERATOR',
     version='0.0.1',
     author='Kanishka Rani',
     author_email='kanishka22043@gmail.com',
-    install_requires=["transformers","accelerate","langchain","streamlit","python-dotenv","PyPDF2","pandas","numpy"],
-    packages=find_packages()
+    description='An MCQ generator using Hugging Face and LangChain',
+    packages=find_packages(),
+    install_requires=[
+        "transformers>=4.40.0",
+        "langchain>=0.1.0",
+        "streamlit",
+        "python-dotenv",
+        "PyPDF2",
+        "pandas",
+        "numpy"
+    ],
+    python_requires=">=3.8",
 )
-
